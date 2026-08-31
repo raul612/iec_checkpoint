64d5100c64f8be61bcdd296aa4d081bed34f243d
JuliaPrin3298 <juliaprinnena1123@gmail.com>
2026-08-31 10:04:49 -0300

    feat: Rota Horarios

--- app.py
+++ app.py
@@ -16,6 +16,15 @@ def filmes_em_cartaz():
     ]
     return jsonify(filmes)
 
+@app.route("/horarios", methods=["GET"])
+def horarios():
+    horarios = [
+        {"filme": "Guerra nas Estrelas: Nova Jornada", "sessoes": ["14:00", "16:30", "19:00", "21:30"]},
+        {"filme": "Comédia da Vida", "sessoes": ["13:00", "15:30", "18:00", "20:15"]},
+        {"filme": "Terror na Noite", "sessoes": ["22:00", "00:00"]},
+    ]
+    return jsonify(horarios)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
